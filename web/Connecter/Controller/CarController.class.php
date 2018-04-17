46cf4425f47b314385c44f9da6fe2d31922e85a3
hayden <hayden.huang@bit-beyond.com>
2018-04-17 11:14:56 +0800

    connecter

---
+++ web/Connecter/Controller/CarController.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Connecter\Controller;
+use Think\Controller;
+class CarController extends Controller {
+    public function index(){
+     	$nav = L('_NAV_');
+     	$footer = L('_FOOTER_');
+     	$home = L('_HOME_');
+     	$this->assign('nav', $nav);
+     	$this->assign('footer', $footer);
+     	$this->assign('home', $home);
+        $this->display('index');
+    }
+    
+    public function lang(){
+	    $wellcome = L('_WELLCOME_');
+	    $this->assign('wellcome', $wellcome);
+	    $this->display();
+	}
+
+	public function get_car(){
+	    $model = M('car');
+        $res = $model -> find();
+
+    	print_r($res);
+
+    	//echo json_encode($data);
+	}
+}
