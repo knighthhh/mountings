04bf4a1624f975c6e787f03762121ba32e2a7f20
hayden <hayden.huang@bit-beyond.com>
2018-04-17 11:02:26 +0800

    index.php config.php

--- web/index.php
+++ web/index.php
@@ -3,19 +3,19 @@
 define("APP_DEBUG", True);
 
 
-define("SITE_URL", "http://139.199.166.127");
+define("SITE_URL", "http://139.199.166.127/mountings/web");
 #define("SITE_URL", "http://127.0.0.1");
 
 //定义前台界面CSS,image,js路径
-define("HOME_CSS_URL", "/Public/Home/css");
-define("HOME_IMAGES_URL", SITE_URL . "/Public/Home/images");
-define("HOME_JS_URL", SITE_URL . "/Public/Home/js");
-define("HOME_PUBLIC", SITE_URL . "/Home/Public");
+define("HOME_CSS_URL", "mountings/web/Public/Home/css");
+define("HOME_IMAGES_URL", SITE_URL . "mountings/web/Public/Home/images");
+define("HOME_JS_URL", SITE_URL . "mountings/web/Public/Home/js");
+define("HOME_PUBLIC", SITE_URL . "mountings/web/Home/Public");
 
 //定义后台界面CSS,image,js路径
-define("ADMIN_CSS_URL", SITE_URL . "/Public/Admin/css/");
-define("ADMIN_IMAGES_URL", SITE_URL . "/Public/Admin/images/");
-define("ADMIN_JS_URL", SITE_URL . "/Public/Admin/js/");
+define("ADMIN_CSS_URL", SITE_URL . "mountings/web/Public/Admin/css/");
+define("ADMIN_IMAGES_URL", SITE_URL . "mountings/web/Public/Admin/images/");
+define("ADMIN_JS_URL", SITE_URL . "mountings/web/Public/Admin/js/");
 
 define("NCBI_URL", "https://www.ncbi.nlm.nih.gov/pubmed/");
 
